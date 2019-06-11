88b769981c3b8018dc1fbf5845a2ef4efaec7b3c
erdendilesmana <47802935+erdendilesmana@users.noreply.github.com>
2019-06-11 09:24:14 +0700

    nambah ama ubah url ke signup jingle

--- Jingl.Web/Startup.cs
+++ Jingl.Web/Startup.cs
@@ -38,12 +38,16 @@ namespace Jingl.Web
                 options.Cookie.HttpOnly = true;
             });
 
+
+
             services.Configure<CookiePolicyOptions>(options =>
             {
                 // This lambda determines whether user consent for non-essential cookies is needed for a given request.
                 options.CheckConsentNeeded = context => false;
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
